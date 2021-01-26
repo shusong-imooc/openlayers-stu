3db63c18fc7da025ec0435f7ee7c41fdca656277
shusong <shusong@hthjsj.com>
2021-01-26 09:35:59 +0800

    圆显示处理

--- src/views/amap/index.vue
+++ src/views/amap/index.vue
@@ -4,6 +4,7 @@
 <script>
 import "ol/ol.css";
 import { get as getProjection } from "ol/proj";
+// import { transform } from "ol/proj";
 import { getWidth, getTopLeft } from "ol/extent";
 import View from "ol/View";
 import Map from "ol/Map";
@@ -13,9 +14,14 @@ import WMTSTileGrid from "ol/tilegrid/WMTS";
 import VectorSource from "ol/source/Vector";
 import VectorLayer from "ol/layer/Vector";
 import Feature from "ol/Feature";
-import GeoJSON from "ol/format/GeoJSON";
+// import GeoJSON from "ol/format/GeoJSON";
 import Point from "ol/geom/Point";
+import LineString from "ol/geom/LineString";
+// import Polygon from "ol/geom/Polygon";
+// import { fromCircle } from "ol/geom/Polygon";
+import Circle from "ol/geom/Circle";
 import olStyle from "ol/style/Style";
+// import olCircle from "ol/style/Circle";
 import olstyleIcon from "ol/style/Icon";
 import olstyleText from "ol/style/Text";
 import olstyleFill from "ol/style/Fill";
@@ -30,7 +36,7 @@ export default {
     return {
       map1: null,
       view: null,
-      vectorSource: null,
+      // vectorSource: null,
       mousePositionControl: null,
     };
   },
@@ -49,13 +55,14 @@ export default {
         matrixIds[z] = z;
       }
       let webKey = "49534f36b1e3ccba7baac6a27ebd2832";
-      let wmtsUrl_1 = "http://t{0-7}.tianditu.gov.cn/vec_c/wmts?tk="; //矢量底图
+      let wmtsUrl_1 = "http://t{0-7}.tianditu.gov.cn/img_c/wmts?tk="; //矢量底图
       let wmtsUrl_2 = "http://t{0-7}.tianditu.gov.cn/cva_c/wmts?tk="; //矢量注记
       this.view = new View({
+        // center: [94.98882144722495, 43.6948398142437],
         center: [94.98882144722495, 43.6948398142437],
         projection: projection,
         // zoom: 8,
-        zoom: 15,
+        zoom: 8,
         maxZoom: 21, // 最大缩放级别
         minZoom: 5, // 最小缩放级别
       });
@@ -107,64 +114,58 @@ export default {
         target: "map",
         view: this.view,
       });
-      this.vectorSource = new VectorSource({
-        features: [],
-      });
-      //初始化矢量图层
-      let vectorLayer = new VectorLayer({
-        source: this.vectorSource,
-      });
-      //将矢量图层添加到map
-      this.map1.addLayer(vectorLayer);
       let point = {
         latitude: "43.70632365345955",
         longitude: "94.97835762798788",
       };
-      let data = { deadRadius: 100 };
+      let data = { deadRadius: 1000 };
       this.showActSimDominoRadiusLayers(point, data);
     },
 
     showActSimDominoRadiusLayers(point, data) {
-      let vectorSource = new VectorSource({
-        format: new GeoJSON(),
-      });
-      let vectorLayer = new VectorLayer({
-        zIndex: 15,
-        visible: false,
-        source: vectorSource,
-      });
-
-      let features = null;
+      let pointFeature, circleFeature, lineFeature;
       // 死亡
       if (data.deadRadius) {
+        console.log(data.deadRadius);
         let color = "rgba(255, 0, 0, 0.6)";
-        let f = this.setCircleFeature(point, data.deadRadius, color);
-        features = f;
+        circleFeature = this.setCircleFeature(point, data.deadRadius, color);
+        pointFeature = this.setPointFeature(point, color);
+        lineFeature = this.setLineFeature(point, color);
       }
-      vectorSource.addFeatures(features);
-      vectorLayer.setVisible(true)
-      this.map.addLayer(vectorLayer);
+      let vectorLayer = new VectorLayer({
+        zIndex: 10,
+        visible: false,
+        source: new VectorSource({
+          features: [pointFeature, circleFeature, lineFeature],
+        }),
+      });
+      vectorLayer.setVisible(true);
+      this.map1.addLayer(vectorLayer);
       return vectorLayer;
     },
-    setCircleFeature(point, radius, color) {
-      // let geom = new ol.geom.Circle(
-      //   [point.longitude, point.latitude],
-      //   radius / 100000
-      // );
-      let geomPoint = new Point([point.longitude, point.latitude]);
-      let f = new Feature({
-        geometry: geomPoint,
-        name: "测试点位",
+    setPointFeature(point, color) {
+      // geom
+      let centerPoint = new Point([point.longitude, point.latitude]);
+      // feature
+      let feature = new Feature({
+        geometry: centerPoint,
+        name: "测试点",
       });
-      f.setStyle(
+      feature.setStyle(
         new olStyle({
           image: new olstyleIcon({
             src: icon,
             scale: 0.6,
             anchor: [0.5, 1],
           }),
+          // image:new olCircle({
+          //   radius:100,
+          //   fill: new olstyleFill({
+          //     color: color,
+          //   }),
+          // }),
           text: new olstyleText({
-            text: name,
+            text: "测试点",
             offsetY: -65,
             font: "14px sans-serif",
             //  标签的背景填充
@@ -179,6 +180,47 @@ export default {
               color: color,
             }),
             padding: [10, 10, 7, 10],
+          }),
+        })
+      );
+      return feature;
+    },
+    setLineFeature(point, color) {
+      let geomLine = new LineString([
+        [point.longitude, point.latitude],
+        [point.longitude, "43.80632365345955"],
+      ]);
+      let f = new Feature({
+        geometry: geomLine,
+        name: "测试点位",
+      });
+      f.setStyle(
+        new olStyle({
+          stroke: new olstyleStroke({
+            color: color,
+          }),
+        })
+      );
+      return f;
+    },
+    setCircleFeature(point, radius, color) {
+      let geomCircle = new Circle(
+        [Number(point.longitude), Number(point.latitude)],
+        // [point.longitude, point.latitude],
+        // transform([point.longitude, point.latitude], 'EPSG:4326', 'EPSG:3857'),
+        radius / 100000
+      );
+      let f = new Feature({
+        geometry: geomCircle,
+        name: "测试点位",
+      });
+      f.setStyle(
+        new olStyle({
+          stroke: new olstyleStroke({
+            color: color,
+          }),
+          fill: new olstyleFill({
+            color: "blue",
           }),
         })
       );
